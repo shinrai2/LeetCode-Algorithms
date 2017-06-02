bb6e80c180d24aa437d924d68280694661742792
shinrai2 <fuck@shinrai.cc>
2017-06-02 12:38:02 +0800

    2017-6-2 12:38:00

---
+++ 12_integer_to_roman.rb
@@ -0,0 +1,9 @@
+# @param {Integer} num
+# @return {String}
+def int_to_roman(num)
+    m = ["", "M", "MM", "MMM"]
+    c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    return m[num/1000] + c[(num%1000)/100] + x[(num%100)/10] + i[num%10]
+end
